6b26e6af57fb86fff240eccba662bd2ab764190f
Act0712 <134161464+Act0712@users.noreply.github.com>
2023-05-21 00:22:46 +0530

    Delete pythonProject13 directory

--- pythonProject13/metalproject/home/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, redirect
-from .models import *
-
-
-def home1(request):
-    return render(request, 'home1.html')
-
--- pythonProject13/metalproject/qualitycontrol/s.py
+++
@@ -1,6 +0,0 @@
-row=5
-for i in range(row):
-    for j in range(i,row-1):
-        print(j, end=' ')
-    print()
-
